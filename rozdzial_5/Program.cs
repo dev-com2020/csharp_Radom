74bb2408e8a8b241943c26e8389ec4234db10399
devcom <kwp@tlen.pl>
2024-06-25 12:48:17 +0200

    Jan Kowalski

--- rozdzial_5/Program.cs
+++ rozdzial_5/Program.cs
@@ -3,8 +3,13 @@
 namespace rozdzial_5
 {
 class Program {
-    static void Main(string[] args) {
-       
+    static void Main(string[] args) 
+    {
+       Employee obj1 = new Employee();
+       obj1.EmployeeId = 1;
+       obj1.FirstName = "Jan";
+       obj1.LastName = "Kowalski";
+       Console.WriteLine(obj1.FirstName);
     }
 }
 }
