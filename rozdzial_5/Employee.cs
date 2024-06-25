0c31da7c3971893dca64217f04a68208fca9b5ad
devcom <kwp@tlen.pl>
2024-06-25 12:37:50 +0200

    pracownik klasa

---
+++ rozdzial_5/Employee.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace rozdzial_5
+{
+    class Employee
+    {
+        public const int StartId = 100;
+        public int EmployeeId;
+        public string FirstName;
+        public string LastName;
+    }
+}
---
+++ rozdzial_5/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace rozdzial_5
+{
+class Program {
+    static void Main(string[] args) {
+       
+    }
+}
+}
